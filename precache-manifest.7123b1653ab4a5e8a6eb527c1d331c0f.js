ed564eb07cbd76db352923cfc7b5fd657a3c6715
flintbean <ajmerando1@yahoo.com>
2018-10-31 15:48:25 -0700

    Updates

---
+++ precache-manifest.7123b1653ab4a5e8a6eb527c1d331c0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "3473647a5592e52575e6",
+    "url": "/Conways-Life/static/js/main.3473647a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Conways-Life/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "3473647a5592e52575e6",
+    "url": "/Conways-Life/static/css/main.2c0bcdc9.chunk.css"
+  },
+  {
+    "revision": "9d71b060f78effa10bc14fdc25d4f50c",
+    "url": "/Conways-Life/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/Conways-Life/precache-manifest.7123b1653ab4a5e8a6eb527c1d331c0f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Conways-Life/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
