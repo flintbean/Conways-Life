29e8ac0be3ee1d8dac808324acb41d0dffd6a871
flintbean <ajmerando1@yahoo.com>
2018-10-31 16:16:15 -0700

    Updates

--- precache-manifest.dfa5434eb477bbdd8536d689d9391c32.js
+++ precache-manifest.dfa5434eb477bbdd8536d689d9391c32.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
   },
   {
-    "revision": "af613c34529e0ca86a30",
-    "url": "/Conways-Life/static/js/main.af613c34.chunk.js"
+    "revision": "1d5baf767da123eaa15c",
+    "url": "/Conways-Life/static/js/main.1d5baf76.chunk.js"
   },
   {
     "revision": "6eb6405c9d99258624bc",
     "url": "/Conways-Life/static/js/1.6eb6405c.chunk.js"
   },
   {
-    "revision": "af613c34529e0ca86a30",
-    "url": "/Conways-Life/static/css/main.1467cb97.chunk.css"
+    "revision": "1d5baf767da123eaa15c",
+    "url": "/Conways-Life/static/css/main.671c0ae8.chunk.css"
   },
   {
-    "revision": "4387e8df0a86d0995b0a9b1e0e5b460c",
+    "revision": "378db88aa995dbf3b3b7157e4bf42f61",
     "url": "/Conways-Life/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/Conways-Life/precache-manifest.3e48dec28112f11f15df6c5c3d36c2e8.js"
+  "/Conways-Life/precache-manifest.dfa5434eb477bbdd8536d689d9391c32.js"
 );
 
 workbox.clientsClaim();
